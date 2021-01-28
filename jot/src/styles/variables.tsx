85f8171f825adaaf3bd7664e962a5dc4bf89a22b
Eugene Golovnov <eugene.golovnov@whykingz.com>
2021-01-28 23:34:14 +0200

    add fonts / add variables / add data.json

---
+++ jot/src/styles/variables.tsx
@@ -0,0 +1,16 @@
+export const colors = {
+    black: '#202125',
+    white: '#FFFFFF',
+    green: '#5BCB02',
+    grey1: '#F8F8F8',
+    grey2: '#EEEEEE',
+    grey3: '#A1A2A4'
+};
+
+export const fontSize = {
+    xSmall: 12,
+    small: 16,
+    medium: 22,
+    large: 24,
+    xLarge: 30
+};
